be1f57443f827f9766a3a8331fd9dbf675ffc1da
ysh <livefor1212@gmail.com>
2021-06-12 10:26:49 +0900

    Initial commit

---
+++ src/main/java/com/memory/space/SpaceApplication.java
@@ -0,0 +1,13 @@
+package com.memory.space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceApplication.class, args);
+	}
+
+}
