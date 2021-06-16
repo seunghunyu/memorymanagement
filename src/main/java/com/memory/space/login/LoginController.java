8193a831c2a5895ff83c4cb975711d3343eae350
ysh <livefor1212@gmail.com>
2021-06-17 02:37:52 +0900

    게시판 메인 html 및 회원가입 폼 html 생성 및 Controller 내용 추가

--- src/main/java/com/memory/space/login/LoginController.java
+++ src/main/java/com/memory/space/login/LoginController.java
@@ -3,17 +3,35 @@ package com.memory.space.login;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/main")
 public class LoginController {
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    //main 로그인 화면 이동
     @GetMapping
     public String main(){
-        logger.info("main page입니다.");
-        return "html/main/login.html";
+        logger.info("main page");
+        return "/html/main/login.html";
     }
+
+    //로그인 기능 수행 -> 로그인 후 게시판이동
+    @PostMapping("/login")
+    public String login(@RequestParam("id") String id,
+                        @RequestParam("password") String password){
+        logger.info("login!!");
+        logger.info("id:"+id+"    password:"+password);
+        return "redirect:/html/board/boardList.html";
+        //redirect 안붙이면 405에러...왜일까....
+    }
+
+    //회원가입 수행
+    @GetMapping("/register")
+    public String register(){
+        logger.info("register!!");
+        return "/html/main/registerForm.html";
+    }
+
 }
