b6016d24e345136735fdf5fe612a67d145ae650f
ysh <livefor1212@gmail.com>
2023-04-20 00:37:55 +0900

    음식점 리스트 ajax 로 replaceWith 처리

--- src/main/java/com/memory/space/restaurant/RestaurantController.java
+++ src/main/java/com/memory/space/restaurant/RestaurantController.java
@@ -4,8 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/restaurant")
@@ -17,6 +20,16 @@ public class RestaurantController {
         logger.info("식당 등록하기 화면");
         return "restaurant/register";
     }
+
+    @RequestMapping(value = "/list", method = RequestMethod.POST)
+    public String list(Model model, String menu){
+        logger.info("식당 리스트 화면");
+        logger.info(menu);
+        //1.food 2.cafe 3.pub
+        //model.addAttribute("menu", menu);
+        return "restaurant/list";
+    }
+
 }
 
 
