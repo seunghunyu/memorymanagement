eece0016b37772f26e3b7aea8179dfe3ddc8e024
ysh <livefor1212@gmail.com>
2023-04-24 23:55:53 +0900

    메뉴 종류에따른 model 변경

--- src/main/java/com/memory/space/restaurant/RestaurantController.java
+++ src/main/java/com/memory/space/restaurant/RestaurantController.java
@@ -5,10 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/restaurant")
@@ -22,10 +19,11 @@ public class RestaurantController {
     }
 
     @RequestMapping(value = "/list", method = RequestMethod.POST)
-    public String list(Model model, String menu){
+    public String list(@RequestParam("name") String menu, Model model){ //ajax로 넘겨받은 data 항목 menu로 받기
         logger.info("식당 리스트 화면");
         logger.info(menu);
         //1.food 2.cafe 3.pub
+        //메뉴에따라 model 조합을 다르게해서 화면에 던지기
         //model.addAttribute("menu", menu);
         return "restaurant/list";
     }
